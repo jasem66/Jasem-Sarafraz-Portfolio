da4fac7d32468290625aef06e4bd6a8e0235103a
jasem66 <jasemsarafraz66@gmail.com>
2022-07-21 18:48:30 +0300

    ch

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BsTwitter, BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import {BsGithub, BsLinkedin } from 'react-icons/bs'
+
 
 const SocialMedia = () => (
   <div className='app__social'>
